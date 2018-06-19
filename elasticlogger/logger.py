458c129a96dd2b5a694af78f5cff8afb152fa822
Willian da Silva <silva.willian@outlook.com>
2018-06-19 19:27:01 -0300

    correcting the stream handler exception

--- elasticlogger/logger.py
+++ elasticlogger/logger.py
@@ -31,19 +31,6 @@ class Logger():
         log = logging.getLogger(index_name_day)
         log.setLevel(logging.DEBUG)
 
-        # create console handler and set level to debug
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.NOTSET)
-
-        # create formatter
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-        # add formatter to ch
-        ch.setFormatter(formatter)
-
-        # add ch to logger
-        log.addHandler(ch)
-
         self.elastic = Elastic(self.id, index_name, index_name_day)
         self.log = log
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Elastic logger',
-    version='1.0.1',
+    version='1.0.2',
     author='Willian da Silva',
     author_email='silva.willian@outlook.com',
     description='Solution created for segregation of logs for the elasticsearch in python',
